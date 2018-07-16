c96a7f14d010e98ec76ab7a226537b22b1e17d7c
MariaVinoth <mariavinothkumar.j@gmail.com>
2018-07-15 22:05:32 -0300

    updated module

--- scrapflask.py
+++ scrapflask.py
@@ -19,35 +19,17 @@ def get_data():
     amount = []
     total = []
 
-    start_price = 'price="'
-    end_price = '" sty'
-    start_id = 'id="md-sell-'
-    end_id = '" price='
-    start_amnt = 'icn-BTC"></i>'
-    end_amnt = '''</td><td class="amount-sum">'''
-    start_total = 'icn-USD"></i>'
-    end_total = '</td></tr>'
-
-
     for data in containers:
-        data1 = str(data)
-        tmp1 = tmp2 = tmp3 = ''
-        tmp1 = data1.split(start_price)
-        for par in tmp1:
-            if end_price in par:
-                price.append(par.split(end_price)[0])
-        tmp2 = data1.split(start_id)
-        for par in tmp2:
-            if end_id in par:
-                id.append(par.split(end_id)[0])
-        tmp3 = data1.split(start_amnt)
-        for par in tmp3:
-            if end_amnt in par:
-                amount.append(par.split(end_amnt)[0])
-        tmp4 = data1.split(start_total)
-        for par in tmp4:
-            if end_total in par:
-                total.append(par.split(end_total)[0])
+        tmp1 = data.findAll('tr')
+        tmp2 = data.findAll('td',{'class':'amount'})
+        tmp3 = data.findAll('td', {'class': 'amount-sum'})
+    for data1 in tmp1:
+        price.append(data1.attrs['price'])
+        id.append(data1.attrs['id'])
+    for data2 in tmp2:
+        amount.append(data2.text)
+    for data3 in tmp3:
+        total.append(data3.text)
 
     browser.close()
     browser.quit()
